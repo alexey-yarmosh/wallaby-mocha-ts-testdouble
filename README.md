84009e8ce4c6602f4bc3d6010b203948ed2b1ef9
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-01-18 11:04:07 +0300

    fix wallaby

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wallaby-mocha-ts-testdouble
 
 `npm test` - to run mocha (3 of 3 tests are passing)
+
+# wallaby fixed issue
+
+https://github.com/wallabyjs/public/issues/3148
--- wallaby.js
+++ wallaby.js
@@ -1,4 +1,8 @@
+import * as path from 'path';
+import * as url from 'url';
+
 export default function (wallaby) {
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   return {
     testFramework: 'mocha',
     files: [
@@ -9,7 +13,20 @@ export default function (wallaby) {
       'tests/**/*.ts'
     ],
     env: {
-      type: 'node'
+      type: 'node',
+      // Tell Wallaby to run your tests with testdouble loader
+      params: {
+        runner: "--experimental-specifier-resolution=node --loader " + 
+          path.join(__dirname, 'node_modules/testdouble/lib/index.mjs')
+      }
+    },
+
+    // Replace file content `.ts` with `.js` because Wallaby transforms
+    // compiles TypeScript files to JavaScript before running them and
+    // so the filenames will be broken in mocks; this is not really safe,
+    // should create a more robust way of replacing filenames in mocks.
+    preprocessors: {
+      '**/*.ts': file => file.content.replace(/\.ts/g, '.js')
     },
     workers: { restart: true },
   };
